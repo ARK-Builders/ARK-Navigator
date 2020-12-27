b39d0c6263f383bd86140b328ff8a849e53888c8
Kirill Taran <kirill@taran.space>
2020-12-28 00:18:46 +0100

    Filename for tags DB changed (hidden file now)

--- app/src/main/java/com/taran/imagemanager/utils/Constants.kt
+++ app/src/main/java/com/taran/imagemanager/utils/Constants.kt
@@ -1,7 +1,7 @@
 package space.taran.arkbrowser.utils
 
 const val DB_NAME = "ArkBrowser.db"
-const val TEXT_STORAGE_NAME = "ArkBrowser.txt"
+const val TEXT_STORAGE_NAME = ".ark-tags"
 const val SHARED_PREFS_NAME = "settings"
 const val PREF_READ_WRITE_GRANTED = "readwrite"
 const val TEMP_URI = "TempURI"
