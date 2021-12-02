2e8c6e8348e2673b7a7497d918696cca2f7826c7
Nazar <loki0376@gmail.com>
2021-12-02 19:00:07 +0100

    Fixed full screen mode glitch

--- app/src/main/java/space/taran/arknavigator/utils/FullscreenHelper.kt
+++ app/src/main/java/space/taran/arknavigator/utils/FullscreenHelper.kt
@@ -2,23 +2,27 @@ package space.taran.arknavigator.utils
 
 import android.view.View
 import android.view.Window
+import android.view.WindowManager
 
 object FullscreenHelper {
-    fun setSystemUIVisibility(isVisible: Boolean, window: Window) {
-        if (isVisible) showSystemUI(window) else hideSystemUI(window)
+    fun setSystemUIVisibility(isVisible: Boolean, window: Window, hasNavigationBar: Boolean) {
+        if (isVisible) showSystemUI(window) else hideSystemUI(window, hasNavigationBar)
     }
 
-    private fun hideSystemUI(window: Window) {
-        window.decorView.systemUiVisibility =
-            (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    private fun hideSystemUI(window: Window, hasNavigation: Boolean) {
+        if (hasNavigation){
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+        else window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 
     private fun showSystemUI(window: Window) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
     }
 }
