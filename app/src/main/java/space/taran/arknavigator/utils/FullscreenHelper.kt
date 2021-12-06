e75ee621d6cfcdd3ed7cf34f7aeb2741202bae71
Nazar <loki0376@gmail.com>
2021-12-06 21:39:30 +0100

    Separate full screen implementation for SDK 30 and higher

--- app/src/main/java/space/taran/arknavigator/utils/FullscreenHelper.kt
+++ app/src/main/java/space/taran/arknavigator/utils/FullscreenHelper.kt
@@ -1,28 +1,48 @@
 package space.taran.arknavigator.utils
 
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.os.Build
+import android.util.Log
+import android.view.*
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 object FullscreenHelper {
-    fun setSystemUIVisibility(isVisible: Boolean, window: Window, hasNavigationBar: Boolean) {
-        if (isVisible) showSystemUI(window) else hideSystemUI(window, hasNavigationBar)
+    fun setSystemUIVisibility(
+        isVisible: Boolean,
+        window: Window,
+        hasNavigationBar: Boolean
+    ) {
+        if (isVisible) showSystemUINew(window) else hideSystemUINew(window, hasNavigationBar)
     }
 
-    private fun hideSystemUI(window: Window, hasNavigation: Boolean) {
-        if (hasNavigation){
-            window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    private fun hideSystemUINew(window: Window, hasNavigation: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowInsetsController = window.insetsController ?: return
+            windowInsetsController.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            windowInsetsController.hide(WindowInsets.Type.systemBars())
+        } else {
+            if (hasNavigation){
+                window.decorView.systemUiVisibility =
+                    (View.SYSTEM_UI_FLAG_IMMERSIVE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            } else window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
-        else window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 
-    private fun showSystemUI(window: Window) {
-        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+    private fun showSystemUINew(window: Window) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowInsetsController = window.insetsController ?: return
+            windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
     }
 }
