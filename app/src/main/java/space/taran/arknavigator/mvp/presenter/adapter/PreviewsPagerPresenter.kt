d8bae8a04eab3c2f1d6b27480694a372ad633750
mdrlzy <mordeniusss@gmail.com>
2022-01-30 04:18:40 +0500

    Gallery init fix

--- app/src/main/java/space/taran/arknavigator/mvp/presenter/adapter/PreviewsPagerPresenter.kt
+++ app/src/main/java/space/taran/arknavigator/mvp/presenter/adapter/PreviewsPagerPresenter.kt
@@ -1,28 +1,28 @@
 package space.taran.arknavigator.mvp.presenter.adapter
 
-import java.nio.file.Path
 import space.taran.arknavigator.mvp.model.repo.index.ResourceMeta
+import space.taran.arknavigator.mvp.model.repo.index.ResourcesIndex
+import space.taran.arknavigator.mvp.model.repo.preview.PreviewAndThumbnail
 import space.taran.arknavigator.mvp.view.GalleryView
 import space.taran.arknavigator.mvp.view.item.PreviewItemView
+import space.taran.arknavigator.utils.ImageUtils
+import space.taran.arknavigator.utils.extension
 
 class PreviewsPagerPresenter(val viewState: GalleryView) {
+    private lateinit var index: ResourcesIndex
     private lateinit var onItemClickListener: (PreviewItemView) -> Unit
     private lateinit var onImageZoomListener: (Boolean) -> Unit
     private lateinit var onPlayButtonListener: () -> Unit
-    private var previews: MutableList<Path?> = mutableListOf()
-    private var placeholders: MutableList<Int> = mutableListOf()
     private var resources: MutableList<ResourceMeta> = mutableListOf()
 
     fun init(
-        previews: List<Path?>,
-        placeholders: List<Int>,
+        index: ResourcesIndex,
         resources: List<ResourceMeta>,
         onItemClickListener: (PreviewItemView) -> Unit,
         onImageZoomListener: (Boolean) -> Unit,
         onPlayButtonListener: () -> Unit
     ) {
-        this.previews = previews.toMutableList()
-        this.placeholders = placeholders.toMutableList()
+        this.index = index
         this.resources = resources.toMutableList()
         this.onItemClickListener = onItemClickListener
         this.onImageZoomListener = onImageZoomListener
@@ -30,18 +30,17 @@ class PreviewsPagerPresenter(val viewState: GalleryView) {
         viewState.updatePagerAdapter()
     }
 
-    fun getCount() = previews.size
+    fun getCount() = resources.size
 
     fun remove(position: Int) {
-        previews.removeAt(position)
-        placeholders.removeAt(position)
         resources.removeAt(position)
     }
 
     fun bindView(view: PreviewItemView) {
-        val preview = previews[view.pos]
-        val placeholder = placeholders[view.pos]
         val resource = resources[view.pos]
+        val path = index.getPath(resource.id)
+        val preview = PreviewAndThumbnail.locate(path, resource)?.preview
+        val placeholder = ImageUtils.iconForExtension(extension(path))
 
         view.setSource(preview, placeholder, resource)
     }
