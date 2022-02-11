4bb3a56914e363c5f597f25d17331fd57a23fe78
Kirill Taran <kirill@taran.space>
2022-02-11 13:47:48 +0200

    Removed TODOs

--- app/src/main/java/space/taran/arknavigator/ui/fragments/GalleryFragment.kt
+++ app/src/main/java/space/taran/arknavigator/ui/fragments/GalleryFragment.kt
@@ -79,7 +79,6 @@ class GalleryFragment : MvpAppCompatFragment(), GalleryView, NotifiableView {
                     val saveFileUri = intent.getStringExtra("RESULT_SAVE_URI")
                     Log.d(GALLERY_SCREEN, "RESULT_ORIGINAL_URI: $originFileUri")
                     Log.d(GALLERY_SCREEN, "RESULT_SAVE_URI: $saveFileUri")
-                    // TODO copy the tags
                 }
             }
         }
