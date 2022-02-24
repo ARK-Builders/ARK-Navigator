83c06282b1ef8136fcaf2dcb44aee7ccecb43836
Kashif2016 <professionalid16@gmail.com>
2022-02-24 05:12:30 +0100

    Jumping from Gallery to Resource Grid with new selection (#196)

--- app/src/main/java/space/taran/arknavigator/mvp/model/UserPreferences.kt
+++ app/src/main/java/space/taran/arknavigator/mvp/model/UserPreferences.kt
@@ -69,6 +69,13 @@ class UserPreferences @Inject constructor(val context: Context) {
         }
     }
 
+    suspend fun setKindTagsEnabled(enabled: Boolean?) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKeys.SHOW_KIND_PREFERENCE] =
+                enabled ?: BuildConfig.DEBUG
+        }
+    }
+
     suspend fun isCrashReportEnabled(): Boolean =
         dataStore.data.first()[PreferencesKeys.CRASH_REPORT]
             ?: DefaultValues.CRASH_REPORT
@@ -93,7 +100,7 @@ class UserPreferences @Inject constructor(val context: Context) {
         dataStore.data.first()[PreferencesKeys.INDEX_REPLICATION]
             ?: DefaultValues.INDEX_REPLICATION
 
-    suspend fun isShowTagsEnabled(): Boolean =
+    suspend fun IsKindTagsEnabled(): Boolean =
         dataStore.data.first()[
             PreferencesKeys.SHOW_KIND_PREFERENCE
         ] ?: SHOW_KIND_PREFERENCE
--- app/src/main/java/space/taran/arknavigator/ui/adapter/TagsSelectorAdapter.kt
+++ app/src/main/java/space/taran/arknavigator/ui/adapter/TagsSelectorAdapter.kt
@@ -2,19 +2,19 @@ package space.taran.arknavigator.ui.adapter
 
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.util.Log
 import androidx.core.content.ContextCompat
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import space.taran.arknavigator.R
 import space.taran.arknavigator.mvp.presenter.adapter.tagsselector.TagsSelectorPresenter
+import space.taran.arknavigator.ui.fragments.GalleryFragment.Companion.KIND_TAGS_PREFIX_KEY
 import space.taran.arknavigator.utils.Tag
 
 class TagsSelectorAdapter(
     private val checkedChipGroup: ChipGroup,
     private val chipGroup: ChipGroup,
     private val presenter: TagsSelectorPresenter,
-    private var newtag: Tag
+    private var selectedTag: Tag
 ) {
     private val clearChip = createClearChip()
     private val chipsByTags = mutableMapOf<Tag, Chip>()
@@ -24,22 +24,13 @@ class TagsSelectorAdapter(
         checkedChipGroup.removeAllViews()
         createChips()
         drawIncludedAndExcludedTags()
-        drawAvailableTags()
         drawClearBtn()
         drawUnavailableTags()
-
-        Log.d("TagSelectorAdpater", "newTag ->" + newtag)
-        presenter.availableTagsForDisplay.forEach { tag ->
-            if (tag.equals(newtag)) {
-                Log.d("TagSelectorAdpater", "both are equal")
-
-                presenter.onTagClick(tag)
-                newtag = ""
-            }
-        }
+        drawAvailableTags()
     }
 
     private fun createChips() {
+        chipsByTags.clear()
         presenter.includedTags.forEach { tag ->
             val chip = createDefaultChip(tag)
             chip.setTextColor(Color.BLUE)
@@ -79,6 +70,11 @@ class TagsSelectorAdapter(
         presenter.availableTagsForDisplay.forEach { tag ->
             chipGroup.addView(chipsByTags[tag])
         }
+        val tag = selectedTag
+        if (selectedTag != "") {
+            selectedTag = ""
+            presenter.onTagClick(tag)
+        }
     }
 
     private fun drawUnavailableTags() {
@@ -97,16 +93,25 @@ class TagsSelectorAdapter(
     }
 
     private fun createDefaultChip(tag: Tag) = Chip(chipGroup.context).apply {
-        Log.d("sendedTag", tag)
         this.isClickable = true
         this.isLongClickable = true
         this.isCheckable = true
         this.isChecked = false
         this.setTextColor(Color.BLACK)
+        if (tag.contains(KIND_TAGS_PREFIX_KEY, ignoreCase = true)) {
+            this.chipBackgroundColor =
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(context, R.color.blue)
+                )
+        } else {
+            this.chipBackgroundColor =
+                ColorStateList.valueOf(
+                    ContextCompat.getColor(context, R.color.grayTransparent)
+                )
+        }
         this.text = tag
 
         this.setOnClickListener {
-            Log.d("I am Click ", tag)
             presenter.onTagClick(tag)
         }
 
