a8367c87e012498b7c3543877892101c05c09684
Parvin Pathan <pgpathan22@gmail.com>
2022-05-26 16:40:40 +0530

    Add new library named as FFmpegMediaMetadataRetriever for display missing metatags of video such as Duration. Also update the all file format's svgs.

--- app/src/main/java/space/taran/arknavigator/mvp/model/repo/kind/VideoKindFactory.kt
+++ app/src/main/java/space/taran/arknavigator/mvp/model/repo/kind/VideoKindFactory.kt
@@ -1,6 +1,6 @@
 package space.taran.arknavigator.mvp.model.repo.kind
 
-import android.media.MediaMetadataRetriever
+import wseemann.media.FFmpegMediaMetadataRetriever
 import android.net.Uri
 import space.taran.arknavigator.mvp.model.repo.index.ResourceId
 import space.taran.arknavigator.ui.App
@@ -8,22 +8,22 @@ import java.nio.file.Path
 
 object VideoKindFactory : ResourceKindFactory<ResourceKind.Video> {
     override val acceptedExtensions: Set<String> =
-        setOf("mp4", "avi", "mkv", "mov", "wmv", "flv")
+        setOf("mp4", "avi", "mkv", "mov", "wmv", "flv", "webm", "ts", "mpg")
     override val acceptedKindCode = KindCode.VIDEO
 
     override fun fromPath(path: Path): ResourceKind.Video {
-        val retriever = MediaMetadataRetriever()
+        val retriever = FFmpegMediaMetadataRetriever()
 
         retriever.setDataSource(App.instance, Uri.fromFile(path.toFile()))
         val durationMillis = retriever
-            .extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            .extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_DURATION)
         val duration = durationMillis?.toLong()
 
         val width = retriever
-            .extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
+            .extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
             ?.toLong()
         val height = retriever
-            .extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
+            .extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
             ?.toLong()
 
         retriever.release()
