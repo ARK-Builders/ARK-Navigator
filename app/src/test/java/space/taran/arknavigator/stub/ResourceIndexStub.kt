34aae4241943a8452fb782c6740199a719eb2362
Dinar Khakimov <85668474+mdrlzy@users.noreply.github.com>
2022-06-02 03:24:45 +0500

    TagSelectorPresenter Unit tests (#243)

---
+++ app/src/test/java/space/taran/arknavigator/stub/ResourceIndexStub.kt
@@ -0,0 +1,27 @@
+package space.taran.arknavigator.stub
+
+import space.taran.arknavigator.mvp.model.repo.index.ResourceId
+import space.taran.arknavigator.mvp.model.repo.index.ResourceMeta
+import space.taran.arknavigator.mvp.model.repo.index.ResourcesIndex
+import java.nio.file.Path
+import kotlin.io.path.Path
+
+class ResourceIndexStub : ResourcesIndex {
+    private val metas = TestData.metasById().toMutableMap()
+
+    override fun listResources(prefix: Path?): Set<ResourceMeta> =
+        metas.values.toSet()
+
+    override fun getPath(id: ResourceId): Path =
+        Path("")
+
+    override fun getMeta(id: ResourceId): ResourceMeta =
+        metas[id]!!
+
+    override suspend fun reindex() {}
+
+    override fun remove(id: ResourceId): Path {
+        metas.remove(id)
+        return Path("")
+    }
+}
---
+++ app/src/test/java/space/taran/arknavigator/stub/TestData.kt
@@ -0,0 +1,62 @@
+package space.taran.arknavigator.stub
+
+import space.taran.arknavigator.mvp.model.repo.index.ResourceMeta
+import space.taran.arknavigator.mvp.model.repo.kind.ResourceKind
+import java.nio.file.attribute.FileTime
+import java.util.Date
+
+object TestData {
+    fun metasById() = mapOf(
+        R1 to ResourceMeta(
+            R1,
+            "Resource1",
+            ".jpg",
+            fileTime(),
+            100,
+            ResourceKind.Image()
+        ),
+        R2 to ResourceMeta(
+            R2,
+            "Resource2",
+            ".jpg",
+            fileTime(),
+            100,
+            ResourceKind.Image()
+        ),
+        R3 to ResourceMeta(
+            R3,
+            "Resource3",
+            ".jpg",
+            fileTime(),
+            120,
+            ResourceKind.Image()
+        ),
+        R4 to ResourceMeta(
+            R4,
+            "Resource4",
+            ".odt",
+            fileTime(),
+            140,
+            ResourceKind.Document()
+        )
+    )
+
+    fun tagsById() = mapOf(
+        R1 to setOf(TAG1, TAG2),
+        R2 to setOf(TAG2),
+        R3 to setOf(),
+        R4 to setOf(TAG3, TAG4)
+    )
+
+    private fun fileTime() = FileTime.from(Date().toInstant())
+}
+
+const val R1 = 1L
+const val R2 = 2L
+const val R3 = 3L
+const val R4 = 4L
+
+const val TAG1 = "tag1"
+const val TAG2 = "tag2"
+const val TAG3 = "tag3"
+const val TAG4 = "tag4"
