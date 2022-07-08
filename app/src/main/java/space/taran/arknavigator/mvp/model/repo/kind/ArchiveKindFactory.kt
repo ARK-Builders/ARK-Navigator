290f7133d08f20e3bd4a22c6983ae96284a272a1
Parvin Pathan <pgpathan22@gmail.com>
2022-07-08 15:30:48 +0530

    Resolved issue after merge.

--- app/src/main/java/space/taran/arknavigator/mvp/model/repo/kind/ArchiveKindFactory.kt
+++ app/src/main/java/space/taran/arknavigator/mvp/model/repo/kind/ArchiveKindFactory.kt
@@ -6,6 +6,9 @@ import space.taran.arknavigator.mvp.model.repo.index.ResourceId
 object ArchiveKindFactory : ResourceKindFactory<ResourceKind.Archive> {
     override val acceptedExtensions: Set<String> =
         setOf("zip", "7z", "rar", "tar.gz", "tar.xz")
+
+    override val acceptedMimeTypes: Set<String>
+        get() = setOf("application/zip")
     override val acceptedKindCode = KindCode.ARCHIVE
 
     override fun fromPath(path: Path) = ResourceKind.Archive()
