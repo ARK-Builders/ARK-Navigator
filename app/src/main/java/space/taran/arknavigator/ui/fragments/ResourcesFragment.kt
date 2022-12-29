7dd233b792f4cfec1a2b19786bfeaeeb642d8a66
ShubertMunthali <munthalimartinz@gmail.com>
2022-12-29 19:33:55 +0200

    Minor fix on shuffle and fav forget

--- app/src/main/java/space/taran/arknavigator/ui/fragments/ResourcesFragment.kt
+++ app/src/main/java/space/taran/arknavigator/ui/fragments/ResourcesFragment.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.annotation.ColorInt
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.FileProvider
 import androidx.core.os.bundleOf
@@ -307,9 +308,12 @@ class ResourcesFragment : MvpAppCompatFragment(), ResourcesView {
         actionBar.btnShuffle.apply {
             setOnClickListener {
                 val dice = this.drawable
+                @ColorInt
+                val diceColor = resources
+                    .getColor(R.color.blue, requireContext().theme)
                 isShuffled = !isShuffled
                 if (isShuffled) {
-                    dice?.setTint(R.color.blue)
+                    dice?.setTint(diceColor)
                     presenter.onShuffleSwitchedOn()
                 } else {
                     dice?.setTintList(null)
