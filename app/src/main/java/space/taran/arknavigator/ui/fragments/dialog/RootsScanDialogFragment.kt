1b0ceae5dad2c17870a60e0ffaf2ef2c486f05c5
huangxin <1229953844@qq.com>
2023-03-29 03:12:24 +0800

    Initialize the app with popular folders #333

--- app/src/main/java/space/taran/arknavigator/ui/fragments/dialog/RootsScanDialogFragment.kt
+++ app/src/main/java/space/taran/arknavigator/ui/fragments/dialog/RootsScanDialogFragment.kt
@@ -1,9 +1,14 @@
 package space.taran.arknavigator.ui.fragments.dialog
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.setFragmentResult
 import moxy.MvpBottomSheetDialogFragment
 import moxy.ktx.moxyPresenter
@@ -13,13 +18,15 @@ import space.taran.arknavigator.mvp.presenter.dialog.RootsScanDialogPresenter
 import space.taran.arknavigator.mvp.view.dialog.RootsScanView
 import space.taran.arknavigator.ui.fragments.utils.toast
 import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.exists
 
 class RootsScanDialogFragment : MvpBottomSheetDialogFragment(), RootsScanView {
     private lateinit var binding: DialogRootsScanBinding
     private val presenter by moxyPresenter {
         RootsScanDialogPresenter()
     }
-
+    private val defaultRootsFolders = listOf("DCIM/Camera", "Documents", "Pictures", "Downloads")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -46,6 +53,34 @@ class RootsScanDialogFragment : MvpBottomSheetDialogFragment(), RootsScanView {
         }
         btnScan.setOnClickListener {
             presenter.onScanBtnClick()
+        }
+        initDefaultRootsInfo(defaultRootsFolders)
+    }
+
+
+    private fun initDefaultRootsInfo(rootsList: List<String>) {
+        rootsList.forEach { root ->
+            val view = CheckBox(context)
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            view.layoutParams = params
+            view.text = root
+            view.isChecked = true
+            val selectColor =
+                ContextCompat.getColor(requireContext(), R.color.colorPrimary)
+            val normalColor = ContextCompat.getColor(requireContext(), R.color.gray)
+            view.setTextColor(selectColor)
+            view.buttonTintList = ColorStateList.valueOf(Color.BLUE)
+            view.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    view.setTextColor(selectColor)
+                } else {
+                    view.setTextColor(normalColor)
+                }
+            }
+            binding.layoutDefaultRoot.addView(view)
         }
     }
 
@@ -76,10 +111,33 @@ class RootsScanDialogFragment : MvpBottomSheetDialogFragment(), RootsScanView {
     override fun closeDialog() = dismiss()
 
     override fun notifyRootsFound(roots: List<Path>) {
+        val finalRoots: MutableList<Path> = mutableListOf()
+        val checkedFolders: MutableList<String> = mutableListOf()
+        val count = binding.layoutDefaultRoot.childCount
+        for (i in 0 until count) {
+            val child = binding.layoutDefaultRoot.getChildAt(i) as CheckBox
+            if (child.isChecked) {
+                checkedFolders.add(defaultRootsFolders[i])
+            }
+        }
+        roots.forEach { root ->
+            var foundDefaultFolder = false
+            checkedFolders.forEach {
+                val defaultRoot = Paths.get(root.toString() + '/' + it)
+                if(defaultRoot.exists()){
+                    finalRoots.add(defaultRoot)
+                    foundDefaultFolder = true
+                }
+            }
+            if(!foundDefaultFolder){
+                finalRoots.add(root)
+            }
+        }
+
         val bundle = Bundle().apply {
             putStringArray(
                 RESULT_KEY_ROOTS,
-                roots.map(Path::toString).toTypedArray()
+                finalRoots.map(Path::toString).toTypedArray()
             )
         }
         setFragmentResult(REQUEST_KEY_ROOTS_FOUND, bundle)
