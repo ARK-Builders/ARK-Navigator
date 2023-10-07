a1491ed8466f40fb00d212bacfd4f3b600887df4
Hieu Vu <star.hieu.99@gmail.com>
2023-10-07 13:21:36 +0700

    Extract method

--- app/src/main/java/dev/arkbuilders/navigator/data/utils/PathExt.kt
+++ app/src/main/java/dev/arkbuilders/navigator/data/utils/PathExt.kt
@@ -1,2 +1,58 @@
 package dev.arkbuilders.navigator.data.utils
 
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.exists
+import kotlin.io.path.extension
+import kotlin.io.path.nameWithoutExtension
+import kotlin.io.path.notExists
+
+val ROOT_PATH: Path = Paths.get("/")
+
+val ANDROID_DIRECTORY: Path = Paths.get("Android")
+
+fun Path.findNotExistCopyName(name: Path): Path {
+    val originalNamePath = this.resolve(name.fileName)
+    if (originalNamePath.notExists())
+        return originalNamePath
+
+    var filesCounter = 1
+
+    fun formatNameWithCounter() =
+        "${name.nameWithoutExtension}_$filesCounter.${name.extension}"
+
+    var newPath = this.resolve(formatNameWithCounter())
+
+    while (newPath.exists()) {
+        newPath = this.resolve(formatNameWithCounter())
+        filesCounter++
+    }
+    return newPath
+}
+
+fun findLongestCommonPrefix(paths: List<Path>): Path {
+    if (paths.isEmpty()) {
+        throw IllegalArgumentException(
+            "Can't search for common prefix among empty collection"
+        )
+    }
+
+    if (paths.size == 1) {
+        return paths.first()
+    }
+
+    fun tailrec(_prefix: Path, paths: List<Path>): Pair<Path, List<Path>> {
+        val grouped = paths.groupBy { it.getName(0) }
+        if (grouped.size > 1) {
+            return _prefix to paths
+        }
+
+        val prefix = _prefix.resolve(grouped.keys.first())
+        val shortened = grouped.values.first()
+            .map { prefix.relativize(it) }
+
+        return tailrec(prefix, shortened)
+    }
+
+    return tailrec(ROOT_PATH, paths).first
+}
