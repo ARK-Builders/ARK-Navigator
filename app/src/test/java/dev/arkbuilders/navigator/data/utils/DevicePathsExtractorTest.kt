7124def97a515079a8a2747be7f2603fdadd2dcc
Hieu Vu <star.hieu.99@gmail.com>
2023-10-08 19:30:17 +0700

    Cover unit test for device paths extractor

--- app/src/test/java/dev/arkbuilders/navigator/data/utils/DevicePathsExtractorTest.kt
+++ app/src/test/java/dev/arkbuilders/navigator/data/utils/DevicePathsExtractorTest.kt
@@ -1,11 +1,52 @@
 package dev.arkbuilders.navigator.data.utils
 
-import org.junit.Test
 
+import dev.arkbuilders.navigator.presentation.App
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+
+@RunWith(MockitoJUnitRunner::class)
 class DevicePathsExtractorTest {
 
-    @Test
-    fun given_when_then() {
+    private val mockedApplication = mockk<App>()
 
+    private lateinit var testee: DevicePathsExtractor
+
+    @BeforeEach
+    fun setUp() {
+        testee = DevicePathsExtractorImpl(
+            appInstance = mockedApplication
+        )
+    }
+
+    @Test
+    fun givenApplicationAvailable_whenGetExternalFileDirs_thenReturnCorrectResult() {
+        val mockedFile = mockk<File>()
+        val files = listOf(mockedFile)
+        every { mockedApplication.getExternalFilesDirs(null) } returns files.toTypedArray()
+        every { mockedFile.exists() } returns true
+
+        val mockedPath = mockk<Path>()
+        every { mockedFile.toPath() } returns mockedPath
+        every { mockedPath.toRealPath() } returns mockedPath
+
+        val directoryIterator: MutableIterator<Path> = arrayListOf(
+            Paths.get("notAndroid")
+
+        ).iterator()
+        every { mockedPath.iterator() } returns directoryIterator
+
+        testee.listDevices()
+
+        verify { mockedApplication.getExternalFilesDirs(null).toList() }
     }
 }
