17e8a05d277ffb0125f2db6515ed242dde4d004b
Dinar Khakimov <85668474+mdrlzy@users.noreply.github.com>
2024-01-03 11:11:03 +0600

    Replace Glide with Coil (#403)
    * Added comments to PreviewImageViewHolder
    * JDK version bumped to 17

--- app/src/main/java/dev/arkbuilders/navigator/presentation/screen/gallery/previewpager/PreviewImageViewHolder.kt
+++ app/src/main/java/dev/arkbuilders/navigator/presentation/screen/gallery/previewpager/PreviewImageViewHolder.kt
@@ -4,9 +4,13 @@ import android.annotation.SuppressLint
 import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.ortiz.touchview.OnTouchImageViewListener
+import dev.arkbuilders.arklib.ResourceId
+import dev.arkbuilders.arklib.data.meta.Metadata
+import dev.arkbuilders.arklib.data.preview.PreviewLocator
+import dev.arkbuilders.arklib.data.preview.PreviewStatus
+import dev.arkbuilders.arklib.utils.ImageUtils
 import dev.arkbuilders.navigator.databinding.ItemImageBinding
 import dev.arkbuilders.navigator.presentation.screen.gallery.GalleryPresenter
 import dev.arkbuilders.navigator.presentation.utils.makeVisibleAndSetOnClickListener
@@ -16,12 +20,6 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import moxy.presenterScope
-import dev.arkbuilders.arklib.ResourceId
-import dev.arkbuilders.arklib.data.meta.Metadata
-import dev.arkbuilders.arklib.data.preview.PreviewLocator
-import dev.arkbuilders.arklib.data.preview.PreviewStatus
-import dev.arkbuilders.arklib.utils.ImageUtils.loadGlideZoomImage
-import dev.arkbuilders.arklib.utils.ImageUtils.loadSubsamplingImage
 import timber.log.Timber
 
 @SuppressLint("ClickableViewAccessibility")
@@ -32,6 +30,7 @@ class PreviewImageViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     private var joinPreviewJob: Job? = null
+    private var subsamplingImageLoadFailed = false
 
     init {
         binding.ivSubsampling.setOnTouchListener { view, motionEvent ->
@@ -95,25 +94,31 @@ class PreviewImageViewHolder(
         }
 
         val path = locator.fullscreen()
-        loadGlideZoomImage(id, path, ivZoom)
-        loadSubsamplingImage(path, ivSubsampling)
+
+        // Loading in parallel limited version of image with cache in ivZoom
+        // and full version in ivSubsampling
+        // User can wait up to several seconds for full version of heavy image
+        ImageUtils.loadImage(id, path, ivZoom, limitSize = true)
+        ImageUtils.loadSubsamplingImage(path, ivSubsampling)
     }
 
     fun reset() = with(binding) {
         progress.isVisible = false
         ivZoom.isVisible = true
         ivZoom.isZoomEnabled = true
+        subsamplingImageLoadFailed = false
     }
 
     fun onRecycled() = with(binding) {
         ivSubsampling.recycle()
-        Glide.with(ivZoom.context).clear(ivZoom)
     }
 
+    // If full version has not yet been loaded, then listen to the zoom, block it
+    // and show progress until it is fully loaded
     private fun setZoomImageEventListener() = with(binding) {
         ivZoom.setOnTouchImageViewListener(object : OnTouchImageViewListener {
             override fun onMove() {
-                if (ivZoom.isZoomed) {
+                if (!subsamplingImageLoadFailed && ivZoom.isZoomed) {
                     progress.isVisible = true
                     ivZoom.isZoomEnabled = false
                     ivZoom.resetZoom()
@@ -131,7 +136,14 @@ class PreviewImageViewHolder(
                     ivZoom.setImageDrawable(null)
                 }
 
-                override fun onImageLoadError(e: Exception?) {}
+                // Fallback to ivZoom
+                // Now this should only happen for .gif and .svg files
+                // Subsampling image can't handle them
+                override fun onImageLoadError(e: Exception?) {
+                    subsamplingImageLoadFailed = true
+                    progress.isVisible = false
+                    ivZoom.isZoomEnabled = true
+                }
 
                 override fun onPreviewLoadError(e: Exception?) {}
 
